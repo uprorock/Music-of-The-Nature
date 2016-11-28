204af7733ef4af6a4d18e9230b6e41c663c1d268
ProRock <uprorock@ya.ru>
2016-11-28 18:47:49 +0300

    Need to do permissions

--- app/src/main/java/com/example/prorock/musicofnature/MainActivity.java
+++ app/src/main/java/com/example/prorock/musicofnature/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.prorock.musicofnature;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.File;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
     OAL test;
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0;
+    int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -21,7 +30,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+*/
         test = new OAL();
         String extStore = Environment.getExternalStorageDirectory().getAbsolutePath();
 
@@ -49,7 +79,24 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Sd Card0 Path",sd0path);
         }
 
-        int indicator = test.play("/mnt/sdcard1/EarthQuake.wav");
+/*
+        String filePath = Environment.getExternalStorageDirectory().toString() + "/EarthQuake.wav";
+        MediaPlayer mp = MediaPlayer.create(this, Uri.parse(filePath));
+        mp.start();
+*/
+/*
+        String path = Environment.getExternalStorageDirectory().toString()+"/Pictures";
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("Files", "Size: "+ files.length);
+        for (int i = 0; i < files.length; i++)
+        {
+            Log.d("Files", "FileName:" + files[i].getName());
+        }
+*/
+        String filePath = Environment.getExternalStorageDirectory().toString() + "/EarthQuake.wav";
+        //int indicator = test.play(filePath);
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(test.stringFromJNI());
